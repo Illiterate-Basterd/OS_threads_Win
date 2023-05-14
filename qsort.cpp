b6c0b7eb728f72d9121880b37cd97197bb28f258
Illiterate-Basterd <ruruabogdan@mil.ru>
2023-05-14 17:40:44 +0300

    The job is done

--- qsort.cpp
+++ qsort.cpp
@@ -2,46 +2,52 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
-#define SWAP(a, b) int temp = a; a = b; b = temp;
+#define SWAP(a, b) \
+	int temp = a;  \
+	a = b;         \
+	b = temp;
 #define MAX(a, b) a > b ? a : b
 #define MIN(a, b) a < b ? a : b
 
 using namespace std;
 
 void quicksort(int *Arr, int first, int last);
-DWORD WINAPI thread_entry(void* param);
+DWORD WINAPI thread_entry(void *param);
+bool check_sort(int *Arr);
 
 HANDLE hMutex;
-int* Array;
+int *Array;
+HANDLE *threads;
 int TCNT, NUM_ELEM, TCNT_C;
+LARGE_INTEGER beg, fin, freq;
+double elapsed = 0.f;
 
 int main(void)
 {
-    fstream input("input.txt", ios::in);
-    if(!input.is_open())
-    {
-        cerr << "Couldn't open the input.txt! Error: " << GetLastError() << endl;
-        return -1;
-    }
+	fstream input("input.txt", ios::in);
+	if (!input.is_open())
+	{
+		cerr << "Couldn't open the input.txt! Error: " << GetLastError() << endl;
+		return -1;
+	}
 
-    fstream output("output.txt", ios::out);
-    if(!output.is_open())
-    {
-        cerr << "Couldn't open the output.txt! Error: " << GetLastError() << endl;
-        return -1;
-    }
+	fstream output("output.txt", ios::out);
+	if (!output.is_open())
+	{
+		cerr << "Couldn't open the output.txt! Error: " << GetLastError() << endl;
+		return -1;
+	}
 
-    fstream time_thread("time.txt", ios::out);
-    if(!time_thread.is_open())
-    {
-        cerr << "Couldn't open the time.txt! Error: " << GetLastError() << endl;
-        return -1;
-    }
+	fstream time_thread("time.txt", ios::out);
+	if (!time_thread.is_open())
+	{
+		cerr << "Couldn't open the time.txt! Error: " << GetLastError() << endl;
+		return -1;
+	}
 
-	size_t beg, end;
 	input >> TCNT >> NUM_ELEM;
 	Array = new int[NUM_ELEM];
-	HANDLE* threads = new HANDLE[TCNT];
+	threads = new HANDLE[TCNT];
 	TCNT_C = NUM_ELEM / TCNT;
 
 	for (size_t i = 0; i < NUM_ELEM; i++)
@@ -49,33 +55,61 @@ int main(void)
 		input >> Array[i];
 	}
 
-	hMutex = CreateMutex(NULL, FALSE, NULL);
+	QueryPerformanceFrequency(&freq);
 
-	beg = GetTickCount64();
-	for(int i = 0; i < TCNT; i++)
+	if (TCNT == 1 || NUM_ELEM <= 1000)
 	{
-		threads[i] = CreateThread(0, 0, thread_entry, (void*)((char*)0 + i), 0, 0);
+		QueryPerformanceCounter(&beg);
+		quicksort(Array, 0, NUM_ELEM - 1);
+		QueryPerformanceCounter(&fin);
+		elapsed += (double)(fin.QuadPart - beg.QuadPart) / freq.QuadPart * 1000;
 	}
-	end = GetTickCount64();
 
-	CloseHandle(hMutex);
-	for(int i = 0; i < TCNT; i++)
+	else
 	{
-		CloseHandle(threads[i]);
-	}
-	quicksort(Array, 0, NUM_ELEM - 1);
-	
 
-	output << TCNT << endl << NUM_ELEM << endl;
-	
+		hMutex = CreateMutex(NULL, FALSE, NULL);
+
+		for (int i = 0; i < TCNT; i++)
+		{
+			threads[i] = CreateThread(0, 0, thread_entry, (void *)((char *)0 + i), 0, 0);
+		}
+
+		while(true)
+		{
+			if(WAIT_TIMEOUT == WaitForMultipleObjects(TCNT, threads, TRUE, 50))
+				continue;
+			else
+			{
+				QueryPerformanceCounter(&beg);
+				quicksort(Array, 0, NUM_ELEM - 1);
+				QueryPerformanceCounter(&fin);
+				elapsed += (double)(fin.QuadPart - beg.QuadPart) / freq.QuadPart * 1000;
+				break;
+			}
+		}
+
+		CloseHandle(hMutex);
+		for (int i = 0; i < TCNT; i++)
+		{
+			CloseHandle(threads[i]);
+		}
+		delete[] threads;
+	}
+
+	output << TCNT << endl
+		   << NUM_ELEM << endl;
+
 	for (size_t i = 0; i < NUM_ELEM; i++)
 	{
 		output << Array[i] << " ";
 	}
 
-	time_thread << end - beg << endl;
+	time_thread << (int)elapsed << endl;
 
-    return 0;
+	//check_sort(Array);
+
+	return 0;
 }
 
 void quicksort(int *Arr, int first, int last)
@@ -107,24 +141,44 @@ void quicksort(int *Arr, int first, int last)
 	}
 }
 
-DWORD WINAPI thread_entry(void* param)
+DWORD WINAPI thread_entry(void *param)
 {
-	if(TCNT == 1 || NUM_ELEM <= 1000)
-	{
-		WaitForSingleObject(hMutex, INFINITE);
-		quicksort(Array, 0, NUM_ELEM - 1);
-		ReleaseMutex(hMutex);
-	}
-	
-	else
-	{
-		WaitForSingleObject(hMutex, INFINITE);
-		int idx = (char*)param - (char*)0;
-		int arg1 = TCNT_C * idx;
-		int arg2 = arg1 + TCNT_C;
+	WaitForSingleObject(hMutex, INFINITE);
 
-		quicksort(Array, MIN(arg1, arg2), MAX(arg1, arg2));
-		ReleaseMutex(hMutex);
-	}
+	int idx = (char *)param - (char *)0;
+	// cout << idx << endl;
+	volatile int arg1 = TCNT_C * idx;
+	volatile int arg2 = arg1 + TCNT_C - 1;
+	//cout << arg1 << " " << arg2 << endl;
+
+	QueryPerformanceCounter(&beg);
+	quicksort(Array, arg1, arg2);
+	QueryPerformanceCounter(&fin);
+	elapsed += (double)(fin.QuadPart - beg.QuadPart) / freq.QuadPart * 1000;
+
+	ReleaseMutex(hMutex);
 	return 0;
+}
+
+bool check_sort(int *Arr)
+{
+	int i;
+	for (i = 1; i < NUM_ELEM; i++)
+	{
+		if (Arr[i - 1] > Arr[i])
+		{
+			cout << "Wrong sort!" << endl;
+			cout << Arr[i - 1] << " " << Arr[i] << endl;
+			return false;
+		}
+	}
+
+	if (i != NUM_ELEM)
+	{
+		cout << "Not enough elements!" << endl;
+		return false;
+	}
+
+	cout << "Sorted" << endl;
+	return true;
 }
